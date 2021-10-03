68d2c587300c74255a95a98f77b8224843708814
Athina Fouseki <80115512+athi-fus@users.noreply.github.com>
2021-10-03 23:32:18 +0300

    Add files via upload

---
+++ global_alignment.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+
+
+def split(word):
+    return list(word)
+
+
+def s(s1i,s2j): #function that tests if two aligning charecters of two strings are the same and calcucates the score
+    if s1i == s2j:
+        return 1
+    else:
+        return -1
+s1=" CCTCGTGAATT" #vertical
+s2="  ΑΑGTACCGGA" #horizontal
+
+colss=split(s2.replace(" ", "_"))
+colss.insert(0," ")
+index = split(s1.replace(" ", "_"))
+index.insert(0, " ")
+
+
+#print(colss)
+
+
+
+#print('Length of s1: {}'.format(len(s1)))
+#print('Length of s2: {}'.format(len(s2)))
+
+rows, cols = (len(s1)+1, len(s2)+1)
+arr = [[100 for i in range(cols)] for j in range(rows)]
+'''
+for i in range(len(s1)+1):
+    for j in range(len(s1)+1):
+        print(arr[i][j], end ="  ")
+    print("\n")
+print("\n\n\n")
+'''
+
+arr[0][0] = 0
+
+for i in range(1, len(s1)+1):
+    arr[0][i]= int(arr[0][i-1]) + int(s(" ", s2[i-1])) #A
+
+for j in range(1, len(s2)+1):
+    arr[j][0]= int(arr[j-1][0]) + int(s(s1[j-1], " ")) #B
+
+
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        #print("i = {}\tj = {}".format(i,j))
+        #print("S1(i):{} \tS2(j):{}".format(s1[i-1], s2[j-1]))
+        #print("arr[i-1][j-1] + s(s1[i-1], s2[j-1]):{}".format(arr[i-1][j-1] + s(s1[i-1], s2[j-1])))
+        #print("arr[i-1][j] + s(s1[i-1], " "):{}".format(arr[i-1][j] + s(s1[i-1], " ")))
+        #print("arr[i][j-1] + s(" ",s2[j-1]):{}\n".format(arr[i][j-1] + s(" ",s2[j-1])))
+        arr[i][j] = max(int(int(arr[i-1][j-1]) + int(s(s1[i-1], s2[j-1]))), int(int(arr[i-1][j]) + int(s(s1[i-1], " "))), int(int(arr[i][j-1]) + int(s(" ",s2[j-1]))))
+'''
+for i in range(len(s1)+1):
+    for j in range(len(s1)+1):
+        print(arr[i][j], end ="  ")
+    print("\n")
+print("\n\n\n")
+'''
+
+
+df = pd.DataFrame(arr, columns=colss, index=index)
+print(df.to_string())
+'''
+for j in range(len(s1)+1):
+    if j == 0:
+        arr[j][0]=s(" ",s2[j-1])
+    else:
+        arr[j][0]=s(" ",s2[j-1]) + sum(arr[0][:j])
+
+for i in range(len(s1)+1):
+    for j in range(len(s1)+1):
+        print(arr[i][j], end ="  ")
+    print("\n")
+#print("\n\n\n") 
+'''
